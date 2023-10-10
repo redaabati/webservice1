7843d6e92288144b5c03016f64417c78373438ec
redaabati <reda.abati@e-polytechnique.ma>
2023-10-10 20:53:22 +0100

    add adsense to website

--- webservice1/pages/_document.js
+++ webservice1/pages/_document.js
@@ -23,6 +23,8 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" data-ad-client="YOUR_CLIENT_ID"></script>
+
         </Head>
         <body>
           <Main />
